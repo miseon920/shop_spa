c72b628b8c6022f00854afaa2f6d11965ca7b652
miseon <miseon920@naver.com>
2022-09-26 10:38:37 +0900

    20220926

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+const company = {
+  name: "my shop",
+  address: "부산시 감전동 미남구 멋짐동 정우성",
+  tel: "010-3333-4444",
+  fax: "051-1004-1004",
+  ceo: "Sunny",
+};
+
+const profile = {
+  slogan: "Hit Hit",
+};
+
+const content = [
+  { id: 1, con: "01슬로건", des: "설명01" },
+  { id: 2, con: "02슬로건", des: "설명02" },
+  { id: 3, con: "03슬로건", des: "설명03" },
+  { id: 4, con: "04슬로건", des: "설명04" },
+  { id: 5, con: "05슬로건", des: "설명05" },
+  { id: 6, con: "06슬로건", des: "설명06" },
+];
+
+export { company, profile, content };
